6aecddffb4af63bce1bff519ea7bd2a0f98e9015
Gerard Hynes <gkhynes@gmail.com>
2018-11-20 21:41:48 +0000

    Refactor changeImages function

--- main.js
+++ main.js
@@ -3,13 +3,6 @@ let arrowLeft = document.querySelector("#arrow-left");
 let arrowRight = document.querySelector("#arrow-right");
 let current = 0;
 
-// Clear all images
-// function reset() {
-//   for (let i = 0; i < sliderImages.length; i++) {
-//     sliderImages[i].style.display = "none";
-//   }
-// }
-
 function reset() {
   sliderImages.forEach(image => {
     image.style.display = "none";
@@ -57,18 +50,14 @@ arrowRight.addEventListener("click", function() {
 function changeImages() {
   if (current === 0) {
     sliderImages[sliderImages.length - 1].style.display = "none";
-    sliderImages[current].style.display = "block";
-    current++;
   } else if (current < sliderImages.length) {
     sliderImages[current - 1].style.display = "none";
-    sliderImages[current].style.display = "block";
-    current++;
   } else {
     current = 0;
     sliderImages[sliderImages.length - 1].style.display = "none";
-    sliderImages[current].style.display = "block";
-    current++;
   }
+  sliderImages[current].style.display = "block";
+  current++;
   setTimeout(() => {
     changeImages();
   }, 3000);
